61a2b520b30095c94986c6477f1ae91672bf24d6
Ali <alikhalibekov2006@gmail.com>
2025-12-29 08:50:49 +0300

    Proj is completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,20 +9,18 @@ import java.nio.file.Path;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String PATH = "C:\\Dev\\projects java\\Course2\\Semestr1\\CG\\TaskObjWriter\\3DModels\\test.obj";
+        String PATH = "C:\\Dev\\projects java\\Course2\\Semestr1\\CG\\TaskObjWriter\\3DModels\\Test07.obj";
         Path fileName = Path.of(PATH);
         String fileContent = Files.readString(fileName);
 
-        System.out.println("Loading model ...");
         Model model = ObjReader.read(fileContent);
 
         System.out.println("Vertices: " + model.vertices.size());
-        System.out.println("Texture vertices: " + model.textureVertices.size());
+        System.out.println("Texture coordinates: " + model.textureVertices.size());
         System.out.println("Normals: " + model.normals.size());
         System.out.println("Polygons: " + model.polygons.size());
 
-        // сохранение модели
-        System.out.println("\nSaving model...");
+        System.out.println();
         ObjWriter.saveModel(model, "output_model.obj");
         System.out.println("Model saved as 'output_model.obj'");
     }
